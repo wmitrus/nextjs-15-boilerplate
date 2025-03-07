60b4240ce15760ab9fc4291ee203d3e65b9bd2b7
Wojciech Mitrus <wmitrus@gmail.com>
2025-03-07 11:53:33 +0100

    chore(eslint): update ESLint configuration
    This commit updates the eslint.config.mjs file to include integration with Prettier and additional ESLint plugins. The configuration extends Next.js, TypeScript, and accessibility (jsx-a11y) recommendations. It also includes rules for Prettier to enforce code formatting standards.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:jsx-a11y/recommended',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['prettier', 'jsx-a11y'],
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
