73c47f4323806665e0044bf08066be3ab68e33b8
Wojciech Mitrus <wmitrus@gmail.com>
2025-08-24 19:04:09 +0200

    feat(sentry): add server and client instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
   const unmocked = [
     'localhost:3000',
@@ -9,6 +11,11 @@ export async function register() {
   ];
 
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+      await import('../sentry.server.config');
+    } else if (process.env.NEXT_RUNTIME === 'edge') {
+      await import('../sentry.edge.config');
+    }
     // MSW INSTRUMENTATION
     const { server } = await import('./lib/mocks/server');
     server.listen({
@@ -24,3 +31,5 @@ export async function register() {
     });
   }
 }
+
+export const onRequestError = Sentry.captureRequestError;
