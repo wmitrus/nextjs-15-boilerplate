354a36d44facb0b0b1942e22f1b19b6e971c2c86
Wojciech Mitrus <wmitrus@gmail.com>
2025-08-27 12:29:26 +0200

    refactor(sentry): adjust tracesSampleRate configuration for production environment

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'test') {
     integrations: [Sentry.replayIntegration()],
 
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 1,
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
 
     // Define how likely Replay events are sampled.
     // This sets the sample rate to be 10%. You may want this to be 100% while
