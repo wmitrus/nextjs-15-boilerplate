165303e4c90e60f319f31d2d72898e1d32d7ef9c
Wojciech Mitrus <wmitrus@gmail.com>
2025-08-27 12:28:11 +0200

    refactor(sentry): improve tracesSampleRate configuration for clarity

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,7 +9,12 @@ Sentry.init({
   dsn: 'https://22fed432630274c1b5547b07c07b8d01@o4508840267612160.ingest.de.sentry.io/4509899911987280',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: process.env.NODE_ENV === 'test' ? 0 : 1,
+  tracesSampleRate:
+    process.env.NODE_ENV === 'test'
+      ? 0
+      : process.env.NODE_ENV === 'production'
+        ? 0.1
+        : 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
