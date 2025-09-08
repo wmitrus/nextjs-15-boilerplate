b8ecb9f9eae933d76ba28c0c65b8b2f3193c6fff
Wojciech Mitrus <wmitrus@gmail.com>
2025-09-08 23:04:40 +0200

    test: silence console warnings and errors during tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,17 @@ import '@testing-library/jest-dom';
 import { matchers } from 'jest-json-schema';
 
 expect.extend(matchers);
+
+// Silence noisy console warnings/errors during tests to avoid failing on expected logs
+let warnSpy: jest.SpyInstance;
+let errorSpy: jest.SpyInstance;
+
+beforeAll(() => {
+  warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterAll(() => {
+  warnSpy?.mockRestore();
+  errorSpy?.mockRestore();
+});
